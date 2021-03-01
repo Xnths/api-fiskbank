2148d3aac00cfe5fb13732548fc613d64ef2e631
xnths <jonathascastilhoes@gmail.com>
2021-03-01 09:13:38 -0300

    Creating Serializer to manege the request data in various forms

---
+++ api/Serializer.js
@@ -0,0 +1,12 @@
+const NotSupported = require('./errors/NotSupported')
+
+class Serializer {
+    _json(data) {
+        return JSON.stringify(data);
+    }
+    serialize(data) {
+        if (this.contentType === "application/json") return this._json(data);
+
+        throw new NotSupported(this.contentType);
+    }
+}
