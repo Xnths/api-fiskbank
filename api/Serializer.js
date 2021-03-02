fc822dbb14adad6c203604f70e7dfcdf6f278d0a
xnths <jonathascastilhoes@gmail.com>
2021-03-02 13:37:23 -0300

    Implementing filter to secure private information

--- api/Serializer.js
+++ api/Serializer.js
@@ -5,15 +5,34 @@ class Serializer {
         return JSON.stringify(data);
     }
     serialize(data) {
-        if (this.contentType === "application/json") return this._json(data);
+        if (this.contentType === "application/json") return this._json(
+            this.filter(data)
+        );
 
         throw new NotSupported(this.contentType);
+    }
+    filterObject(data) {
+        const newObject = {};
+        this.publicParams.forEach(param => {
+            if (data.hasOwnProperty(param)) newObject[param] = data[param];
+        })
+        return newObject;
+    }
+    filter(data) {
+        if (Array.isArray(data)) {
+            data = data.map(item => this.filterObject(item));
+        } else {
+            data = this.filterObject(data);
+        }
+
+        return data;
     }
 }
 class SerializerStudent extends Serializer {
     constructor(contentType) {
         super();
         this.contentType = contentType;
+        this.publicParams = ['id', 'name'];
     }
 }
 module.exports = {
