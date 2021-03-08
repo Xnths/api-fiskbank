a9fcfbb97a90967562fa9f8252b61d7317047999
xnths <jonathascastilhoes@gmail.com>
2021-03-08 12:38:41 -0300

    Dropping previous table and creating a new one with operationID, which identifies the operations

--- api/routes/students/transactions/ModelTransactionsTable.js
+++ api/routes/students/transactions/ModelTransactionsTable.js
@@ -1,7 +1,7 @@
 const query = require('../../../database/query');
 
 const create = () => {
-    const sql = "CREATE TABLE IF NOT EXISTS Transactions (id INT NOT NULL REFERENCES Students(id), date DATETIME DEFAULT CURRENT_TIMESTAMP, amount FLOAT NOT NULL)"
+    const sql = "CREATE TABLE IF NOT EXISTS Transactions (operationID INT NOT NULL AUTO_INCREMENT, date DATETIME DEFAULT CURRENT_TIMESTAMP, amount FLOAT NOT NULL, id INT NOT NULL, CONSTRAINT fk_StudentId FOREIGN KEY (id) REFERENCES Students(id), PRIMARY KEY (operationID))"
     return query(sql);
 }
 
