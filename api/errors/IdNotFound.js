35d05fb3e42e2b0713077eefbd283ba76ce3f9c6
xnths <jonathascastilhoes@gmail.com>
2021-03-10 09:22:23 -0300

    Changing error to be adaptable

--- api/errors/IdNotFound.js
+++ api/errors/IdNotFound.js
@@ -1,6 +1,6 @@
 class IdNotFound extends Error {
-    constructor(id) {
-        super(`There is no Student with id ${id}`);
+    constructor(entity, id) {
+        super(`There is no ${entity} with id ${id}`);
         this.name = "IdNotFound";
         this.idError = 0;
     }
