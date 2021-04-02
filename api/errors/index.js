980bc94c70f040b371808a33e096175e15a7600b
xnths <jonathascastilhoes@gmail.com>
2021-04-02 18:41:19 -0300

    Creating indexes

---
+++ api/errors/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    EmptyLog: require('./EmptyLog'),
+    IdNotFound: require('./IdNotFound'),
+    InsufficientFunds: require('./InsufficientFunds'),
+    InvalidParam: require('./InvalidParam'),
+    NegativeNumber: require('./NegativeNumber'),
+    NotEligibleParam: require('./NotEligibleParam'),
+    NotSupported: require('./NotSupported'),
+    PositiveNumber: require('./PositiveNumber'),
+    UnauthorizedCommand: require('./UnauthorizedCommand')
+}
